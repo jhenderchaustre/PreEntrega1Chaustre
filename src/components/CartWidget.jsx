0b8903bbedc97cb813a38cc702d1bd8f4e6eb934
jhenderchaustre <jhenderchaustrear@gmail.com>
2023-12-03 16:52:18 -0300

    first commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+const CartWidget = () => {
+    return (
+<div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '35px'}}>
+    🛒
+    <span style={{fontWeight: 'bold', fontSize: '1rem', color: '#008491'}}>1</span>
+</div>
+    )
+}
+
+export default CartWidget;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = () => {
+    return (
+        <div style={{textAlign: 'center', marginTop: '230px', fontSize: '3rem'}}>
+            Bienvenidos a Rhino Store®
+            <p style={{fontSize: '1rem'}}>Estamos creando nuestro sitio👷‍♂️🔧</p>
+            </div>
+    )
+} 
+export default ItemListContainer;
